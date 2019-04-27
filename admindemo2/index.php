2b8232c7dc6f1c65e5723c81a9c4d65f9a3860ee
Thanh-Tran-Thi <tranthanh.031298@gmail.com>
2019-04-27 17:16:49 +0700

    admin demo 2

---
+++ admindemo2/index.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();  
+
+include "./templates/top.php"; 
+
+?>
+ 
+<?php include "./templates/navbar.php"; ?>
+
+<div class="container-fluid">
+  <div class="row">
+    
+    <?php include "./templates/sidebar.php"; ?>
+
+  </div>
+</div>
+
+<?php include "./templates/footer.php"; ?>
+
+<script type="text/javascript" src="./js/admin.js"></script>
